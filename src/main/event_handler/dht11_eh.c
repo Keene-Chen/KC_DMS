9bd91a24d321b9a7fc396b3f7f60e69e5e56fc8a
KeeneChen <2421985222@qq.com>
2023-05-18 10:43:07 +0800

    🦄 refactor: 优化事件函数逻辑

--- src/main/event_handler/dht11_eh.c
+++ src/main/event_handler/dht11_eh.c
@@ -6,13 +6,11 @@ void dht11_handler(void* client, message_data_t* msg)
 
     // MQTT_LOG_I("%s:%d %s()...\r\ntopic: %s\r\nmessage:%s\r\n", __FILE__, __LINE__, __FUNCTION__,
     //            msg->topic_name, (char*)msg->message->payload);
-    yyjson_doc* doc =
-        yyjson_read((char*)msg->message->payload, strlen((char*)msg->message->payload), 0);
-
+    yyjson_doc* doc = yyjson_read((char*)msg->message->payload, strlen(msg->message->payload), 0);
     if (doc) {
         yyjson_val* temp = yyjson_obj_get(doc->root, "temp");
         yyjson_val* humi = yyjson_obj_get(doc->root, "humi");
-        if (yyjson_get_real(temp) > 31 || yyjson_get_real(humi) > 70) {
+        if (yyjson_get_real(temp) > TEMP_ALERT || yyjson_get_real(humi) > HUMI_ALERT) {
             open_dev(BLUE_LED);
         }
         else {
--- src/main/event_handler/fire_eh.c
+++ src/main/event_handler/fire_eh.c
@@ -6,12 +6,10 @@ void fire_handler(void* client, message_data_t* msg)
     // MQTT_LOG_I("%s:%d %s()...\r\ntopic: %s\r\nmessage:%s\r\n", __FILE__, __LINE__, __FUNCTION__,
     //            msg->topic_name, (char*)msg->message->payload);
 
-    yyjson_doc* doc =
-        yyjson_read((char*)msg->message->payload, strlen((char*)msg->message->payload), 0);
-
+    yyjson_doc* doc = yyjson_read((char*)msg->message->payload, strlen(msg->message->payload), 0);
     if (doc) {
         yyjson_val* fire = yyjson_obj_get(doc->root, "raw");
-        if (yyjson_get_int(fire) < 4000) {
+        if (yyjson_get_int(fire) < FIRE_ALERT) {
             open_dev(RED_LED);
             open_dev(BEEP_DEV);
         }
